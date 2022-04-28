e10eb4fc347b6a79fc9f9b89a379965a6235f90c
苏南 <me@sunan.me>
2022-04-28 11:01:22 +0800

    测试极端 修改配置文件

--- core/consensus/constants.py
+++ core/consensus/constants.py
@@ -1,6 +1,6 @@
 # 最初的想法是这里根据总票数进行一个计算，但是似乎平均每1024个众生区块出一个时代区块也很合理，先这样。
-VotesOfTimesBlockGenerate = 4.0
-VotesOfGarbageBlockGenerate = 4.0
+VotesOfTimesBlockGenerate = 32.0
+VotesOfGarbageBlockGenerate = 32.0
 # 长期票有效时间为128个选举周期
 LongTermVoteValidityPeriod = 128
 # 规定的节点申请或删除审核时间（大约五天时间）
